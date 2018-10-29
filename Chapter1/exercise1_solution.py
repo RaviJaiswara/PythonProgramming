9eded4a57d6f74209299f47caab64bebb240a125
Jaiswararavi <ravijaiswara92@gmail.com>
2018-10-30 00:42:35 +0530

    chapter1

---
+++ Chapter1/exercise1_solution.py
@@ -0,0 +1,11 @@
+# ************* print these following lines *****
+# this is \\ double backslash
+# these are /\/\/\/\/\ mountains
+# he is    awesome (use escape sequence instead of manual spaces)
+# \" \n \t \' (print thsi as an output)
+
+
+print("this is \\\\ double backslash")
+print("these are /\/\/\/\/\ maountains")
+print("he is\tawesome")
+print("\\\" \\n \\t \\\'")
